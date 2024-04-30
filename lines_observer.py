abfb9516c80ec154f4b1070a9643518be1b385c0
Alexandr Belyy <sashabelyy123@gmail.com>
2024-04-30 21:56:24 +0500

    Исправил файл lines_observer

--- lines_observer.py
+++ lines_observer.py
@@ -3,17 +3,12 @@ import pandas as pd
 import datetime
 
 class LineObserver:
-    
-    class_dict = {2:'car',3:'motorcycle',5:'bus',7:'truck'}
     columns=["date","from","to","class"]
 
     def __init__(self):
         self.lines = dict()
         self.target_objects = dict()
-        self.id = 0
-
         self.row_list = []
-        self.info_table = pd.DataFrame() 
         self.date_table = pd.DataFrame()
         
 
@@ -22,6 +17,7 @@ class LineObserver:
         self.lines[self.id] = line
     
     def upd_directions(self,obj_id):
+        # Если линий две то делаем запись в таблицу
         if len(self.target_objects[obj_id]['lines'])==2:
             
             line_id1, line_id2 = self.target_objects[obj_id]['lines'][0], self.target_objects[obj_id]['lines'][1]
@@ -32,35 +28,23 @@ class LineObserver:
             values = (datetime.datetime.now(), line_id1, line_id2, class_name)
             self.row_list.append(dict(zip(LineObserver.columns, values)))
             self.date_table = pd.DataFrame(self.row_list, columns=LineObserver.columns)
-            print(self.date_table)
             
 
     def update(self, detections:sv.Detections):
+        # Добавляем к объекту номер линии которую он пересек
         for lz_id, lz in self.lines.items():
             crossed_in, crossed_out = lz.trigger(detections)
 
-            for obj_id, class_id in zip(detections[crossed_in].tracker_id, detections[crossed_in].class_id):
-                if obj_id not in self.target_objects.keys():
-                    self.target_objects[obj_id] = {'lines':[lz_id], 'class_id':class_id}
-                else:
-                    self.target_objects[obj_id]['lines'].append(lz_id)
-                self.upd_directions(obj_id)
-                
-            
-            for obj_id, class_id in zip(detections[crossed_out].tracker_id, detections[crossed_out].class_id):
-                if obj_id not in self.target_objects.keys():
-                    self.target_objects[obj_id] = {'lines':[lz_id], 'class_id':class_id}
-                else:
-                    self.target_objects[obj_id]['lines'].append(lz_id)
-                self.upd_directions(obj_id)
+            for direction in (crossed_in,crossed_out):
+                objects_id = detections[direction].tracker_id
+                classes_id = detections[direction].class_id
 
-          
-
-            #print(detections[crossed_in].tracker_id)
-            #print(detections[crossed_out].tracker_id)
-    def info(self):
-        print(self.info_table)
-    
+                for obj_id, class_id in zip(objects_id, classes_id):
+                    if obj_id not in self.target_objects.keys():
+                        self.target_objects[obj_id] = {'lines':[lz_id], 'class_id':class_id}
+                    else:
+                        self.target_objects[obj_id]['lines'].append(lz_id)
+                    self.upd_directions(obj_id)
     
     
             
